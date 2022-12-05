c6f1a1a9044186b4232acf46a7f14367cd7f0d59
gustavofranco26 <gustavo260123@gmail.com>
2022-12-04 23:14:02 -0500

    Adding the changues in file to task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Return the information about an employee from an API
 whose ID is passed into the script"""
-
 from requests import get
 from sys import argv
 
@@ -9,18 +8,18 @@ from sys import argv
 def api_api():
     """Data struct to api """
     employee_id = int(argv[1])
-    employee_name = ""
+    employee_name = ''
     number_of_done_tasks = 0
     number_of_tasks = 0
     titles_of_tasks = []
 
-    users_res = get("https://jsonplaceholder.typicode.com/users").json()
+    users_res = get('https://jsonplaceholder.typicode.com/users').json()
     for user in users_res:
         if user['id'] == employee_id:
             employee_name = user['name']
             break
 
-    tasks_res = get("https://jsonplaceholder.typicode.com/todos").json()
+    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
     for task in tasks_res:
         if task['userId'] == employee_id:
             if task['completed']:
@@ -32,7 +31,7 @@ def api_api():
                                                           number_of_done_tasks,
                                                           number_of_tasks))
     for title in titles_of_tasks:
-        print("\t {}".format(title))
+        print('\t {}'.format(title))
 
 
 if __name__ == '__main__':
